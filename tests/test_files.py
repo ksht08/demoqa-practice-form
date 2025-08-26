4b9e5c15d417d0ec196e4e8c0feb8174d47715eb
ksht08 <ksht08@proton.me>
2025-08-26 17:08:53 +0300

    update test_files.py

--- tests/test_files.py
+++ tests/test_files.py
@@ -5,11 +5,12 @@ import time
 import os, requests
 import pytest
 
-# 'tests\' - enter the 'tests' folder
-# '..' - go up one level (to the root of the project)
+# '__file__' - absolute path to the current file
+# '..' - go up one level
 # 'TMP' - enter the 'TMP' folder
 TMP_DIR = os.path.join(os.path.dirname(__file__), '..', 'TMP')
 README_FILE = os.path.join(TMP_DIR, "README.rst")
+FILE1 = "new file.txt"
 
 @pytest.fixture(scope='function', autouse=True)
 def local_browser_setup():
@@ -31,7 +32,7 @@ def local_browser_setup():
     browser.config.browser_name = "chrome"
     config.window_width = 1500
     config.window_height = 1024
-    browser.open("/")
+    
     yield
     browser.quit()
 
@@ -43,8 +44,16 @@ def cleanup_after_all_tests():
         file_path = os.path.join(TMP_DIR, file)
         if os.path.isfile(file_path):
             os.remove(file_path)
+    
+    # check that TMP directory is empty
+    tmp_is_empty = (len(os.listdir(TMP_DIR)) == 0)
+    if tmp_is_empty:
+        print("\n✅ TMP folder successfully cleaned!")
+    else:
+        print("\n⚠️ TMP folder still contains files!")
 
 def test_download_readme_file():
+    browser.open("/")
     # readme_raw_button = browser.element("[data-testid='raw-button']").locate()
     # readme_raw_url = readme_raw_button.get_attribute("href")
     readme_raw_url = browser.element("[data-testid='raw-button']").get(query.attribute("href"))
@@ -62,9 +71,40 @@ def test_read_readme_file():
         assert "https://github.com/pytest-dev/pytest/blob/main/LICENSE" in file_content
 
 def test_create_file():
-    file_path = os.path.join(TMP_DIR, "new file.txt")
+    file_path = os.path.join(TMP_DIR, FILE1)
     with open(file_path, "w") as f:
         f.write("hello!\nHow are you?")
     with open(file_path) as f:
         file_content = f.read()
         assert "How are you" in file_content
+
+def test_create_folder():
+    folder_path = os.path.join(TMP_DIR, "new folder")
+    os.makedirs(folder_path, exist_ok=True)
+    assert os.path.exists(folder_path)
+
+def test_edit_file():
+    file_path = os.path.join(TMP_DIR, FILE1)
+    with open(file_path, "a") as f:
+        f.write("\n\nI am fine, thank you!\n")
+    with open(file_path) as f:
+        file_content = f.read()
+        assert "I am fine," in file_content
+
+def test_check_files_path():
+    # '__file__' - absolute path to the current file
+    # '..' - go up one level (to the root of the project)
+    project_dir = os.path.join(os.path.dirname(__file__), '..')
+    tmp_dir = os.path.join(os.path.dirname(__file__), '..', 'TMP')
+    current_dir = os.path.dirname(__file__)
+    print(f"\nProject directory: {project_dir}\nTMP directory: {tmp_dir}\nCurrent directory: {current_dir}\n")
+
+def test_get_file_names():
+    files_list = []
+    for file in os.listdir(TMP_DIR):
+        file_path = os.path.join(TMP_DIR, file) # get the file name
+        if os.path.isfile(file_path): # check if it's a file in TMP folder
+            files_list.append(file)
+    print("\nFiles in TMP folder:")
+    for file in files_list:
+        print(f"{file}")

a1a6c636fb118616b5c9193d1ff2907467bbb34c
ksht08 <ksht08@proton.me>
2025-09-09 14:11:43 +0300

    update tests/test_demoqa_practice_form.py: add Allure markup

--- tests/test_demoqa_practice_form.py
+++ tests/test_demoqa_practice_form.py
@@ -1,4 +1,5 @@
 import pytest
+import allure
 from selene import browser
 from selene.support.shared import config
 from pages.demoqa_registration_form import RegistrationPage
@@ -14,31 +15,56 @@ def local_browser_setup():
     yield
     browser.quit()
 
+allure.feature("DemoQA Practice Form")
+allure.story("User Registration Form Submission")
+@allure.title("Test User Registration Form")
+@allure.description("""
+This test fills out and submits the user registration form
+on DemoQA and verifies the submission.
+""")
 def test_registration_form():
     registration_page = RegistrationPage()
 
-    registration_page.open()
-    registration_page.check_h1(user.h1)
-    registration_page.fill_first_name(user.first_name)
-    registration_page.fill_last_name(user.last_name)
-    registration_page.fill_email(user.email)
-    registration_page.select_gender(user.gender)
-    registration_page.fill_mobile_number(user.mobile)
-    registration_page.fill_date_of_birth(user.date_of_birth["year"],
-                                           user.date_of_birth["month"],
-                                           user.date_of_birth["day"],
-                                           user.date_of_birth_short)
-    registration_page.select_subjects(user.subjects)
-    registration_page.select_hobbies(user.hobbies[1], user.hobbies[2])
-    registration_page.upload_picture(user.picture_path)
-    registration_page.submit_form()
-    registration_page.assert_user_registration_info(
-        user.full_name,
-        user.email,
-        user.gender,
-        user.mobile,
-        user.subjects,
-        user.hobbies[1],
-        user.hobbies[2],
-        user.picture
+    with allure.step("Open registration page"):
+        registration_page.open()
+
+    with allure.step("Check h1 header"):
+        registration_page.check_h1(user.h1)
+
+    with allure.step("Fill out and submit the form"):
+        registration_page.fill_first_name(user.first_name)
+        registration_page.fill_last_name(user.last_name)
+        registration_page.fill_email(user.email)
+        registration_page.select_gender(user.gender)
+        registration_page.fill_mobile_number(user.mobile)
+        registration_page.fill_date_of_birth(user.date_of_birth["year"],
+                                             user.date_of_birth["month"],
+                                             user.date_of_birth["day"],
+                                             user.date_of_birth_short)
+        registration_page.select_subjects(user.subjects)
+        registration_page.select_hobbies(user.hobbies[1], user.hobbies[2])
+        registration_page.upload_picture(user.picture_path)
+        allure.attach(
+            browser.driver.get_screenshot_as_png(),
+            name="Filled Form Screenshot",
+            attachment_type=allure.attachment_type.PNG
+            )
+        
+    with allure.step("Submit the form"):
+        registration_page.submit_form()
+    with allure.step("Check submission results"):
+        registration_page.assert_user_registration_info(
+            user.full_name,
+            user.email,
+            user.gender,
+            user.mobile,
+            user.subjects,
+            user.hobbies[1],
+            user.hobbies[2],
+            user.picture
         )
+        allure.attach(
+            browser.driver.get_screenshot_as_png(),
+            name="Modal Form with Filled Data Screenshot",
+            attachment_type=allure.attachment_type.PNG
+            )
